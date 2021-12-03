4db24a10fd1d6d3858ec2994752f4c55ffcc32b5
Amauri Oliveira <amauriibate32@hotmail.com>
2021-12-03 16:15:35 -0300

    Ajuste no model de Fornecedor

--- app/Fornecedor.php
+++ app/Fornecedor.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Fornecedor extends Model
 {
-    //
+    protected $table = 'fornecedores'; // para acertar o plural, ele espera que a tabela chame 'fornecedors'
+    protected $fillable = ['nome', 'site', 'uf', 'email']; // para poder colocar registro de forma '::create'
 }
